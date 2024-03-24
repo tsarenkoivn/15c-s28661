6baa469a1ad8064e9566400eb4cb1a50870dc58f
itamizxc <tsarenko04y@gmail.com>
2024-03-24 16:05:24 +0100

    task2 done

--- lab3.py
+++ lab3.py
@@ -1,3 +1,12 @@
-#task1
-numlist = [x**2 for x in range(11)]
+# task1
+numlist = [x ** 2 for x in range(11)]
 print(numlist)
+
+
+# task2
+def e_squares(start, end):
+    squarelist = [x ** 2 for x in range(start, end + 1)]
+    return squarelist
+
+
+print(e_squares(2, 10))

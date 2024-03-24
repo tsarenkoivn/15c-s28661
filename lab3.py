2934b74c2d49f16ccf27c6e8fa2d85628c37ada3
itamizxc <tsarenko04y@gmail.com>
2024-03-24 16:19:29 +0100

    task3 done

--- lab3.py
+++ lab3.py
@@ -11,3 +11,12 @@ def e_squares(start, end):
 print(e_squares(2, 10))
 
 
+# task3
+
+class SquareGenerator:
+    def squares(self, start, end):
+        return [x ** 2 for x in range(start, end + 1)]
+
+
+a = SquareGenerator()
+print(a.squares(3, 10))
